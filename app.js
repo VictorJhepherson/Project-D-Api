ea44014c23589081b4968cb50d2a9d486a91011d
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-08-24 19:42:01 -0300

    24.08.21 - Ajustes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     );
 
     if(res.method === 'OPTIONS'){ 
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
         return res.status(200).send({});
     }
     next();
