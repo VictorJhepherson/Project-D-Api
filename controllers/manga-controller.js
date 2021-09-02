6eeb799e6110eb22e15e8d2447ff0c061ac89c97
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-02 20:42:40 -0300

    02.09.21 - registerManga and db-version

--- controllers/manga-controller.js
+++ controllers/manga-controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const fs = require('fs');
 
 exports.getAll = (req, res, next) => {
     mysql.getConnection((error, conn) => {
@@ -42,5 +43,38 @@ exports.getByName = (req, res, next) => {
             
             return res.status(200).send({ data: results[0] });
         });
+    });
+};
+
+exports.registerMangas = (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'CALL REGISTER_MANGAS(?);', 
+            [ req.body.MG_TITLE ],
+            (error, result, field) => {
+                if(result.length == 0) {
+                    res.status(409).send({ mensagem: 'Não foi possível cadastrar o mangá'})
+                } else {
+                    fs.readdir(req.body.MGC_PATH, (err, files) => {
+                        if(err) { return res.status(500).send({ error: err }) }
+                        files.forEach(file => {
+                            conn.query(
+                                'CALL REGISTER_CHAPTERS(?, ?);', 
+                                [ result[0].MG_ID, req.body.MGC_PATH + file ],
+                                (error, result, field) => {
+                                    conn.release();
+                                    if(error) { res.status(500).send({ error: error }) }
+                    
+                                    return res.status(201).send({
+                                        mensagem: 'Mangá criado com sucesso'
+                                    });
+                                }
+                            );
+                        });
+                    });
+                }
+            }
+        );
     });
 };
--- routes/manga.js
+++ routes/manga.js
@@ -8,5 +8,6 @@ const MangaController = require('../controllers/manga-controller');
 router.get('/', login, MangaController.getAll);
 router.get('/:MG_ID', login, MangaController.getById);
 router.post('/byName', login, MangaController.getByName);
+router.post('/', login, MangaController.registerMangas);
 
 module.exports = router;
