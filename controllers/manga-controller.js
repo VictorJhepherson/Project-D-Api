0b3e8469e11966f4e42712ef0b4095a242bc4131
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-02 21:48:35 -0300

    02.09.21 - registerManga ajuste6

--- controllers/manga-controller.js
+++ controllers/manga-controller.js
@@ -56,19 +56,16 @@ exports.registerMangas = (req, res, next) => {
                 if(error) {
                     res.status(409).send({ error: error, mensagem: 'Não foi possível cadastrar o mangá'})
                 } else {
-                    console.log(result.insertId);
-                    fs.readdir(req.body.MG_PATH, (err, files) => {
-                        if(err) { conn.rollback();  return res.status(500).send({ error: err, mensagem: 'Leitura da pasta' }) }
+                    const path = req.body.MG_PATH;
+                    fs.readdir(path, (err, files) => {
+                        if(err) { return res.status(500).send({ error: err, mensagem: 'Leitura da pasta' }) }
                         files.forEach(file => {
                             conn.query(
                                 'CALL REGISTER_CHAPTERS(?, ?);', 
                                 [ result.insertId, file ],
                                 (error, result, field) => {
-                                    if(error) { 
-                                        conn.rollback(); 
-                                        conn.release();
-                                        return res.status(500).send({ error: error, mensagem: 'REGISTER_CHAPTERS' }) 
-                                    }
+                                    conn.release();
+                                    if(error) { return res.status(500).send({ error: error, mensagem: 'REGISTER_CHAPTERS' }) }
                     
                                     return res.status(201).send({
                                         mensagem: 'Mangá criado com sucesso'
