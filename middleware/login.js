c55ed78ec5aaeb18a5b23127362f92fe9a73e7ac
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-10 13:44:34 -0300

    10/09/21 - Upload PDF ajuste1

--- middleware/login.js
+++ middleware/login.js
@@ -2,10 +2,6 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        res.header('Access-Control-Allow-Origin', '*'),
-        res.header('Access-Control-Allow-Headers', 
-            'Content-Type, Origin, X-Requested-With, Accept, Authorization'
-        );
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, process.env.JWT_KEY);
         req.user = decode;
