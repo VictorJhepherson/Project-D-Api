c7f9194ae81f252d6292e821a7c12dc106907ae7
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-13 19:54:43 -0300

    13/09/21 - UserController

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -99,36 +99,3 @@ exports.registerUsers = (req, res, next) => {
         })
     });
 };
-
-exports.editUser = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if(error) { return res.status(500).send({ error: error }) }
-        conn.query(`SELECT SU_LOGINNAME
-                      FROM SYSTEMUSERS
-                     WHERE SU_LOGINNAME = ?`, [req.body.SU_LOGINNAME], (error, results) => {
-            if(error) { return res.status(500).send({ error: error }) }
-            if(results.length > 0){
-                res.status(409).send({ mensagem: 'E-mail já cadastrado', error: 0 })
-            } else {
-                conn.query(
-                    'CALL EDIT_SYSTEMUSERS(?, ?, ?, ?, ?, ?);', 
-                    [
-                        req.body.SU_NICKNAME, req.body.SU_LOGINNAME, 
-                        req.body.SU_PHONENUMBER, req.body.SU_DATEBIRTHDAY, 
-                        req.body.SU_PHOTO, req.body.SU_ID
-                    ],
-                    (error, result, field) => {
-                        conn.release();
-                        if(error) { res.status(500).send({ error: error }) }
-        
-                        let token = jwt.sign({ SU_NICKNAME: req.body.SU_NICKNAME }, process.env.JWT_KEY, { expiresIn: "7d" });  
-                        return res.status(201).send({
-                            mensagem: 'Usuário editado com sucesso',
-                            token: token
-                        });
-                    }
-                );
-            }
-        });
-    });
-};
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ const login = require('../middleware/login');
 const UserController = require('../controllers/user-controller');
 
 router.get('/getUser/:id', login, UserController.getUserById);
+router.post('/edit', login, UserController.editUser);
 
 module.exports = router;
