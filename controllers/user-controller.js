9ab359a4d1cc8c5abeedb213a2beab20ba1f2f27
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-13 20:00:16 -0300

    13/09/21 - UserController 2

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,9 +7,9 @@ exports.getUserById = (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if(error) { return res.status(500).send({ error: error }) }
         const query = `SELECT * 
-                         FROM systemusers
-                        WHERE SU_ID= ?`;
-        conn.query(query, [req.params.user], (error, results, fields) => {
+                         FROM SYSTEMUSERS
+                        WHERE SU_ID = ?`;
+        conn.query(query, [req.body.SU_ID], (error, results, fields) => {
             conn.release();
             if(error) { return res.status(500).send({ error: error }) }
             
