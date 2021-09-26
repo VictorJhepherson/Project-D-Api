73bcf3192de2546228163e38f0c2517adb6b8e4f
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-26 17:44:31 -0300

    26.09.21 - Ajustes e testes 6

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const rotaManga = require('./routes/manga');
 const rotaUser = require('./routes/user');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
