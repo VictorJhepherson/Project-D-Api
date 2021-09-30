a4369f25a4b2746525a99b3ddf8b5387690af2df
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-09-30 11:43:18 -0300

    30.09.21 - Teste Upload image 1

--- routes/manga.js
+++ routes/manga.js
@@ -5,11 +5,32 @@ const login = require('../middleware/login');
 
 const MangaController = require('../controllers/manga-controller');
 
+const storage = multer.memoryStorage({
+    destination: function (req, file, cb){
+        cb(null, '');
+    },
+    filename: function(req, file, cb){
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'application/pdf'){
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter
+});
 
 router.get('/', login, MangaController.getAll);
 router.get('/:MG_ID', login, MangaController.getById);
 router.post('/byName', login, MangaController.getByName);
-router.post('/title', login, MangaController.registerMangas);
+router.post('/title', upload.single('MGC_ARCHIVE'), login, MangaController.registerMangas);
 router.post('/chapters', login, MangaController.registerChapters);
 
 module.exports = router;
