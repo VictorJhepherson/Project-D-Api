fc72cb385b16248467c9cd36cd6861c7e01d46d8
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-10-02 15:53:57 -0300

    02.10.21 - Atualizações

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -34,25 +34,76 @@ exports.editUser = (req, res, next) => {
             if(results.length > 0){
                 res.status(409).send({ success: false, mensagem: 'E-mail já cadastrado' })
             } else {
-                conn.query(
-                    'CALL EDIT_SYSTEMUSERS(?, ?, ?, ?, ?);', 
-                    [
-                        req.body.SU_NICKNAME, req.body.SU_LOGINNAME, 
-                        req.body.SU_PHONENUMBER, req.body.SU_PHOTO, req.body.SU_ID
-                    ],
-                    (error, result, field) => {
-                        conn.release();
-                        if(error) { res.status(500).send({ success: false, mensagem: 'Não foi possível editar o usuário', error: error }) }
-        
-                        let token = jwt.sign({ SU_NICKNAME: req.body.SU_NICKNAME }, process.env.JWT_KEY, { expiresIn: "7d" });  
-                        return res.status(201).send({
-                            success: true,
-                            mensagem: 'Usuário editado com sucesso',
-                            token: token
-                        });
+                conn.query(`SELECT SU_LOGINNAME
+                              FROM SYSTEMUSERS
+                             WHERE SU_ID = ?
+                               AND SU_LOGINNAME = ?`, [req.body.SU_ID, req.body.SU_LOGINNAME], 
+                    (error, results) => {
+                        if(error) { return res.status(500).send({ success: false, mensagem: 'Não foi possível verificar se o email a ser editado é igual ao antigo', error: error }) }
+                        if(results.length > 0) {
+                            conn.query(
+                                'CALL EDIT_SYSTEMUSERS(?, ?, ?, ?);', 
+                                [
+                                    req.body.SU_NICKNAME, null, 
+                                    req.body.SU_PHONENUMBER, req.body.SU_ID
+                                ],
+                                (error, result, field) => {
+                                    conn.release();
+                                    if(error) { res.status(500).send({ success: false, mensagem: 'Não foi possível editar o usuário', error: error }) }
+                    
+                                    let token = jwt.sign({ SU_NICKNAME: req.body.SU_NICKNAME }, process.env.JWT_KEY, { expiresIn: "7d" });  
+                                    return res.status(201).send({
+                                        success: true,
+                                        mensagem: 'Usuário editado com sucesso',
+                                        token: token
+                                    });
+                                }
+                            );
+                        } else {
+                            conn.query(
+                                'CALL EDIT_SYSTEMUSERS(?, ?, ?, ?);', 
+                                [
+                                    req.body.SU_NICKNAME, req.body.SU_LOGINNAME, 
+                                    req.body.SU_PHONENUMBER, req.body.SU_ID
+                                ],
+                                (error, result, field) => {
+                                    conn.release();
+                                    if(error) { res.status(500).send({ success: false, mensagem: 'Não foi possível editar o usuário', error: error }) }
+                    
+                                    let token = jwt.sign({ SU_NICKNAME: req.body.SU_NICKNAME }, process.env.JWT_KEY, { expiresIn: "7d" });  
+                                    return res.status(201).send({
+                                        success: true,
+                                        mensagem: 'Usuário editado com sucesso',
+                                        token: token
+                                    });
+                                }
+                            );
+                        }
                     }
                 );
             }
+        });
+    });
+};
+
+exports.editPassword = (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error) { return res.status(500).send({ success: false, mensagem: 'Não foi possível iniciar conexão com o banco de dados', error: error}) }
+        bcrypt.hash(req.body.SU_PASSWORD, 10, (errBcrypt, hash) => {
+            if(errBcrypt){ return res.status(500).send({ success: false, mensagem: 'Não foi possível realizar o Bcrypt da senha', error: errBcrypt }) }
+            conn.query(
+                'CALL EDIT_PASSWORD(?, ?)',
+                [ req.body.SU_ID, hash ],
+                (error, result, field) => {
+                    conn.release();
+                    if(error) { res.status(500).send({ success: false, mensagem: 'Não foi possível realizar a atualização', error: error }) }
+    
+                    res.status(202).send({
+                        success: true,
+                        mensagem: 'Senha atualizada com sucesso'
+                    });
+                }
+            )
         });
     });
 };
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@ const UserController = require('../controllers/user-controller');
 
 router.get('/getUser/:user', login, UserController.getUserById);
 router.post('/edit', login, UserController.editUser);
+router.post('/editPass', login, UserController.editPassword);
 
 module.exports = router;
