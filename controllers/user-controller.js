4c5c884f0d1115095dde1510d894e0a38b71de04
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-10-05 21:29:17 -0300

    05.10.21 - Ajuste

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.getUserById = (req, res, next) => {
